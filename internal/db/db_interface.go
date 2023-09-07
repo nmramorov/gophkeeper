3810f4c61cb36e7c63fce8fd475b4b3137fc3f59
Nikita Mramorov <nmramorov98@gmail.com>
2023-09-08 01:00:04 +0300

    Refactored db interfaces a bit

--- internal/db/db_interface.go
+++ internal/db/db_interface.go
@@ -6,10 +6,18 @@ import (
 	"github.com/nmramorov/gophkeeper/internal/api/models"
 )
 
-type DBAPI interface {
+type Credentials interface {
 	SaveCredentials(ctx context.Context, data models.CredentialsData) error
 	LoadCredentials(ctx context.Context, id string) (models.CredentialsData, error)
+}
+
+type Auth interface {
 	SaveUser(ctx context.Context, data models.User) error
 	LoadUser(ctx context.Context, token string) (models.User, error)
+}
+
+type DBAPI interface {
+	Credentials
+	Auth
 	Ping(ctx context.Context) error
 }
