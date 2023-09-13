7bc6f5baaf8c24951fdb1ef47a67564f4353aeaf
Nikita Mramorov <nmramorov98@gmail.com>
2023-09-13 16:16:56 +0300

    fixed server test

--- internal/api/server/grpc_server_test.go
+++ internal/api/server/grpc_server_test.go
@@ -3,42 +3,19 @@ package server
 import (
 	"context"
 	"log"
-	"net"
 	"testing"
 
 	"google.golang.org/grpc"
 
-	"github.com/nmramorov/gophkeeper/internal/db"
 	pb "github.com/nmramorov/gophkeeper/internal/proto"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/test/bufconn"
 )
-
-const bufSize = 1024 * 1024
-
-var lis *bufconn.Listener
-
-func init() {
-	lis = bufconn.Listen(bufSize)
-	s := grpc.NewServer()
-	pb.RegisterStorageServer(s, &StorageServer{
-		Storage: &db.InMemoryDB{},
-	})
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			log.Fatalf("Server exited with error: %v", err)
-		}
-	}()
-}
-
-func bufDialer(context.Context, string) (net.Conn, error) {
-	return lis.Dial()
-}
 
 func TestStorageServer(t *testing.T) {
 	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	go Run(ctx)
+	conn, err := grpc.DialContext(ctx, ":3200", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,6 +23,7 @@ func TestStorageServer(t *testing.T) {
 
 	client := pb.NewStorageClient(conn)
 
+	// register new user
 	response, errResp := client.RegisterUser(ctx, &pb.RegisterUserRequest{
 		User: &pb.User{
 			Login:    "stray228",
@@ -54,4 +32,5 @@ func TestStorageServer(t *testing.T) {
 	})
 	require.NoError(t, errResp)
 	require.Equal(t, "", response.Error)
+
 }
